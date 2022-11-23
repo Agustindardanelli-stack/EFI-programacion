dbb43cc93812553698d80db273370c5e37cd34ca
Valentín Gomez <v.gomez@itecriocuarto.org.ar>
2022-11-23 19:27:43 -0300

    Contact y Login fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,75 +1,54 @@
-import React, { useState } from "react";
+import React from 'react'
 import { useContext } from 'react'
 import { LanguageContext } from '../context/Context'
 import { langtranslate } from '../locales/chooseLang'
 
-const FORM_ENDPOINT = "";
 const ContactForm = () => {
-  const { language } = useContext(LanguageContext);
-  const textsLanguage = langtranslate(language,'contact')
-  const [submitted, setSubmitted] = useState(false);
-  const handleSubmit = () => {
-    setTimeout(() => {
-      setSubmitted(true);
-    }, 100);
-  };
-  if (submitted) {
-    return (
-      <>
-        <div className="text-2xl">Thank you!</div>
-        <div className="text-md">We'll be in touch soon.</div>
-      </>
-    );
-  }
+  const { language } = useContext(LanguageContext)
+  const textsLanguage = langtranslate(language, 'contact')
+
   return (
-    <div
-      className="h-screen flex flex-col 
-      items-center justify-center"
-    >
-      <div className="mb-3 pt-0">
-        <h3 className="text-center text-gray-400 text-s">{textsLanguage.text}</h3>
+    <div className='flex flex-col items-center justify-center h-auto  bg-gray-600'>
+      <div className=' pt-0'>
+        <h3 className='text-center text-gray-400 text-s mt-9'>{textsLanguage.text}</h3>
       </div>
-      <form
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-        action={FORM_ENDPOINT}
-        onSubmit={handleSubmit}        
-      >
-        <div className="mb-3 pt-0">
+      <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-28 mt-28'>
+        <div className='mb-3 pt-0'>
           <input
-            type="text"
+            type='text'
             placeholder={textsLanguage.name}
-            name="name"
-            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+            name='name'
+            className='px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full'
             required
           />
         </div>
-        <div className="mb-3 pt-0">
+        <div className='mb-3 pt-0'>
           <input
-            type="email"
+            type='email'
             placeholder={textsLanguage.email}
-            name="email"
-            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+            name='email'
+            className='px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full'
             required
           />
         </div>
-        <div className="mb-3 pt-0">
+        <div className='mb-3 pt-0'>
           <textarea
             placeholder={textsLanguage.message}
-            name="message"
-            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+            name='message'
+            className='px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full'
             required
           />
         </div>
-        <div className="mb-3 pt-0">
+        <div className='mb-3 pt-0'>
           <button
-            className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-            type="submit"
+            className='bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+            type='submit'
           >
             {textsLanguage.submit}
           </button>
         </div>
       </form>
     </div>
-  );
-};
-export default ContactForm;
+  )
+}
+export default ContactForm
